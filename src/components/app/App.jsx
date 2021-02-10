612bfb90d2798e94c7677546a73b039f76ac60d8
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-10 14:18:00 -0800

    added files to sort pathing

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route>
+          
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
