01d0d3dc9ce42a24e316dc4d053565d2f6093c6f
ryanleviathan <ryan.m.carreras@gmail.com>
2021-02-10 15:13:55 -0800

    passing all tests

--- src/containers/ByCharacterQuotes.test.jsx
+++ src/containers/ByCharacterQuotes.test.jsx
@@ -1,2 +1,17 @@
-import { Component } from "react"
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import ByCharacterQuotes from './ByCharacterQuotes';
 
+describe('ByCharacterQuotes container', () => {
+  it('displays quotes by a character', async() => {
+    render(<ByCharacterQuotes match={{ params: { character: 'Bender' } }} />);
+
+    screen.getByAltText('Loading');
+
+    const ul = await screen.findByTestId('quotes');
+
+    return waitFor(() => {
+      expect(ul).not.toBeEmptyDOMElement();
+    });
+  });
+});
